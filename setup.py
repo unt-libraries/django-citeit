7f96e7a56cdb6979ed7537e3207cd8ef99d3d920
Gio Gottardi <ManuelGGottardi@gmail.com>
2015-07-28 15:06:46 -0500

    Filled in setup.py.

---
+++ setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='citeit',
+    version='0.1.0',
+    packages=find_packages(exclude=['tests*']),
+    description='A Django app for the creation of an annotated bibliography.'
+    long_description=('Please visit https://github.com/unt-libraries/citeIt '
+                      'for the latest documentation.'),
+    include_package_data=True,
+    url='https://github.com/unt-libraries/citeIt',
+    author='University of North Texas Libraries',
+    author_email='mark.phillips@unt.edu',
+    license='BSD',
+    keywords=['django', 'annotated', 'bibliography'],
+    classifiers=[
+        'Natural Language :: English',
+        'Environment :: Web Environment',
+        'Framework :: Django :: 1.8',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7'
+    ]
+)
