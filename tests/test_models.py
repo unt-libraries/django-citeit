c1a3e02f27a42b9897437314a93ef6952df55dd0
Gio Gottardi <ManuelGGottardi@gmail.com>
2015-07-30 16:43:43 -0500

    Reverted to creating instances of the models directly in test_models.py rather than using the model factories.

--- tests/test_models.py
+++ tests/test_models.py
@@ -2,7 +2,7 @@ from unittest import expectedFailure
 
 from django.test import TestCase
 
-from . import factories
+from citeIt.models import Institution, DegreeLevel, Subject, Location, Citation
 
 
 class TestModelMethods(TestCase):
@@ -10,9 +10,18 @@ class TestModelMethods(TestCase):
 
     def test_full_title_with_initial_article(self):
         """Check that the initial article gets prepended to the title."""
-        citation_with_article = factories.CitationFactory(
+        institution = Institution.objects.create(
+            institution_abbreviation='UNT')
+        degree = DegreeLevel.objects.create(
+            degree_abbreviation='MBA')
+        citation_with_article = Citation.objects.create(
+            withers_thesis_number='1',
             initial_article='The',
-            title='Traitor'
+            title='Traitor',
+            pagination='42',
+            granting_institution=institution,
+            degree_level=degree,
+            year='1998',
         )
         self.assertEqual(citation_with_article.full_title(), 'The Traitor')
 
@@ -21,37 +30,69 @@ class TestModelMethods(TestCase):
     @expectedFailure
     def test_full_title_without_initial_article(self):
         """Check that title with no initial article gets printed correctly."""
-        factories.CitationFactory(initial_article='', title='Politicians')
-        self.assertEqual(self.without_article.full_title(), 'Politicians')
+        institution = Institution.objects.create(
+            institution_abbreviation='UNT')
+        degree = DegreeLevel.objects.create(
+            degree_abbreviation='MBA')
+        citation_without_article = Citation.objects.create(
+            withers_thesis_number='1',
+            title='Politicians',
+            pagination='42',
+            granting_institution=institution,
+            degree_level=degree,
+            year='1998',
+        )
+        self.assertEqual(citation_without_article.full_title(), 'Politicians')
 
     def test_pagination_pretty(self):
         """Check that the page number is correctly appended with a 'pp'."""
-        citation = factories.CitationFactory(pagination='42')
+        institution = Institution.objects.create(
+            institution_abbreviation='UNT')
+        degree = DegreeLevel.objects.create(
+            degree_abbreviation='MBA')
+        citation = Citation.objects.create(
+            withers_thesis_number='1',
+            pagination='42',
+            granting_institution=institution,
+            degree_level=degree,
+            year='1998',
+        )
         self.assertEqual(citation.pagination_pretty(), '42pp')
 
     def test_institution_str_repr(self):
         """Check that the model's string representation is the abbreviation."""
-        institution = factories.InstitutionFactory(
+        institution = Institution.objects.create(
             institution_abbreviation='UNT')
         self.assertEqual(str(institution), 'UNT')
 
     def test_degreelevel_str_repr(self):
         """Check that the model's string representation is the abbreviation."""
-        degree_level = factories.DegreeLevelFactory(degree_abbreviation='MBA')
+        degree_level = DegreeLevel.objects.create(degree_abbreviation='MBA')
         self.assertEqual(str(degree_level), 'MBA')
 
     def test_subject_str_repr(self):
         """Check that the model's string representation is the subject."""
-        subject = factories.SubjectFactory(subject='Alchemy')
+        subject = Subject.objects.create(subject='Alchemy')
         self.assertEqual(str(subject), 'Alchemy')
 
     def test_location_str_repr(self):
         """Check that the model's string representation is the location."""
-        location = factories.LocationFactory(location='Russia')
+        location = Location.objects.create(location='Russia')
         self.assertEqual(str(location), 'Russia')
 
     def test_citation_str_repr(self):
         """Check that the model's string representation is the title."""
-        citation = factories.CitationFactory(initial_article='The',
-                                             title='Wandering Mouse')
+        institution = Institution.objects.create(
+            institution_abbreviation='UNT')
+        degree = DegreeLevel.objects.create(
+            degree_abbreviation='MBA')
+        citation = Citation.objects.create(
+            withers_thesis_number='1',
+            initial_article='The',
+            title='Wandering Mouse',
+            pagination='42',
+            granting_institution=institution,
+            degree_level=degree,
+            year='1998',
+        )
         self.assertEqual(str(citation), 'Wandering Mouse')
