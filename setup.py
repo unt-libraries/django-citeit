ceb0ac724b6e0eba4975853979e1e17f2d315e98
Gio Gottardi <ManuelGGottardi@gmail.com>
2015-09-01 14:08:53 -0500

    Changed version in setup.py to 1.0.0

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='django-citeit',
-    version='0.1.0',
+    version='1.0.0',
     packages=find_packages(exclude=['tests*']),
     description='A Django app for the creation of an annotated bibliography.',
     long_description=('Visit https://github.com/unt-libraries/django-citeit '
