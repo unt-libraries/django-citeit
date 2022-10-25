0606b4b84cfd9db4e407531f548d3e8a1bfc16d0
gracieflores <53153376+gracieflores@users.noreply.github.com>
2022-10-25 14:54:24 -0500

    remove py36 py37, add py38 py39, upgrade django4.1

--- setup.py
+++ setup.py
@@ -16,11 +16,11 @@ setup(
     classifiers=[
         'Natural Language :: English',
         'Environment :: Web Environment',
-        'Framework :: Django :: 3.2',
+        'Framework :: Django :: 4.1',
         'Programming Language :: Python',
         'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.5'
-        'Programming Language :: Python :: 3.6'
-        'Programming Language :: Python :: 3.7'
+        'Programming Language :: Python :: 3.8'
+        'Programming Language :: Python :: 3.9'
+        
     ]
 )
